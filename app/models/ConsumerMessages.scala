28951c2d24eac1e6340ddc4b1824cabf66feedfa
Sean Policarpio <sean.policarpio@simplemachines.com.au>
2017-07-11 22:37:23 +1000

    added ConsumerMessages and corresponding JSON formats

---
+++ app/models/ConsumerMessages.scala
@@ -0,0 +1,161 @@
+package models
+
+import java.util.Properties
+
+import actors.consumer.ConsumerActor.{AvroConsumer, ConsumerType, StringConsumer}
+import actors.consumer.{AtOffset, FromBeginning, FromEnd, OffsetPosition}
+import org.apache.kafka.clients.consumer.{ConsumerRecord, ConsumerRecords}
+import org.apache.kafka.common.TopicPartition
+import play.api.libs.json._
+
+import scala.collection.JavaConverters._
+
+object ConsumerMessages {
+
+  sealed trait ConsumerMessage
+
+  case class KafkaConsumerError(reason: String, error: Option[Throwable] = None) extends ConsumerMessage
+
+  case class ConsumerConfig(consumerId: String, props: Properties, consumerType: ConsumerType)
+  case class CreateConsumer(conf: ConsumerConfig) extends ConsumerMessage
+
+  case object ShutdownConsumer extends ConsumerMessage
+
+  case class StartConsumer(topic: String, offsetStart: OffsetPosition) extends ConsumerMessage
+
+  case class MoveOffset(topic: String, partition: Int, offsetPosition: OffsetPosition) extends ConsumerMessage
+
+  case class MessagesPayload[K,V](payload: ConsumerRecords[K, V]) extends ConsumerMessage
+
+  object jsonformats {
+
+    implicit val ThrowableFormat: Format[java.lang.Throwable] = Format(
+      Reads[java.lang.Throwable]({
+        case JsObject(map) => map.get("throwable").fold[JsResult[java.lang.Throwable]](JsError("Not a java.lang.Throwable"))(reason => JsSuccess(new Throwable(reason.as[String])))
+        case _ => JsError("Not a java.lang.Throwable")
+      }),
+      Writes[java.lang.Throwable](t => JsObject(Map("throwable" -> JsString(t.getMessage))))
+    )
+
+    implicit val OptionalThrowableFormat: Format[Option[java.lang.Throwable]] = Format(
+      Reads[Option[java.lang.Throwable]]({
+        case JsNull => JsSuccess(None)
+        case js => ThrowableFormat.reads(js).map(t => Some(t))
+      }),
+      Writes[Option[java.lang.Throwable]](_ => JsNull)
+    )
+
+    implicit val ConsumerTypeFormat = Format(
+      Reads[ConsumerType]({
+        case JsString("StringConsumer") => JsSuccess(StringConsumer)
+        case JsString("AvroConsumer") => JsSuccess(AvroConsumer)
+        case _ => JsError("Not a ConsumerType")
+      }),
+      Writes[ConsumerType]({
+        case StringConsumer => JsString("StringConsumer")
+        case AvroConsumer => JsString("AvroConsumer")
+      })
+    )
+
+    implicit val JavaPropertiesFormat = Format(
+      Reads[java.util.Properties] {
+        case JsObject(props) =>
+          val javaProps = new  java.util.Properties()
+          props.foreach {
+            case (k, v) => javaProps.setProperty(k, v.as[String])
+          }
+          JsSuccess(javaProps)
+        case _ => JsError("Not a java.util.Properties object")
+      },
+      Writes[java.util.Properties](p => {
+        JsObject(p.propertyNames().asScala.map(_.toString).map(name => name -> JsString(p.getProperty(name))).toMap[String, JsValue])
+      })
+    )
+
+    implicit val ConsumerConfigFormat: Format[ConsumerConfig] = Json.format[ConsumerConfig]
+    implicit val OffsetPosition: Format[OffsetPosition] = Format(
+      Reads[OffsetPosition]({
+        case JsString("FromBeginning") => JsSuccess(FromBeginning)
+        case JsString("FromEnd") => JsSuccess(FromEnd)
+        case atOffset: JsNumber => JsSuccess(AtOffset(atOffset.as[Long]))
+        case _ => JsError("Invalid OffsetPosition")
+      }),
+      Writes[OffsetPosition]({
+        case FromBeginning => JsString("FromBeginning")
+        case FromEnd => JsString("FromEnd")
+        case AtOffset(offset) => JsNumber(offset)
+      })
+    )
+
+    implicit val TopicPartitionFormat: Format[TopicPartition] = Format(
+      Reads[TopicPartition]({
+        case JsString(topicPartition) =>
+          val split = topicPartition.split("][")
+          try {
+            JsSuccess(new TopicPartition(split.head, split.tail.reverse.head.toInt))
+          } catch {
+            case t: Throwable => JsError(t.getMessage)
+          }
+        case _ => JsError("Invalid TopicPartition")
+      }),
+      Writes[TopicPartition](tp => JsString(s"${tp.topic()}][${tp.partition()}"))
+    )
+
+    implicit val ConsumerRecordWrites: Writes[ConsumerRecord[_, _]] = Writes { cr =>
+      JsObject {
+        Map(
+          "key" -> JsString(cr.key().toString),
+          "value" -> JsString(cr.value().toString),
+          "offset" -> JsNumber(cr.offset())
+        )
+      }
+    }
+
+    implicit val KafkaConsumerErrorFormat: Format[KafkaConsumerError] = Json.format[KafkaConsumerError]
+    implicit val CreateConsumerFormat: Format[CreateConsumer] = Json.format[CreateConsumer]
+    implicit val StartConsumerFormat: Format[StartConsumer] = Json.format[StartConsumer]
+    implicit val MoveOffsetFormat: Format[MoveOffset] = Json.format[MoveOffset]
+
+    implicit val MessagesPayloadWrites: Writes[MessagesPayload[_, _]] = Writes { mp =>
+      JsObject {
+        Seq(
+          "payload" ->
+            JsObject {
+              mp.payload.partitions().asScala.map(topicPartition => {
+                val jsArray = JsArray(mp.payload.records(topicPartition).asScala.sortBy(_.offset()).map(ConsumerRecordWrites.writes))
+                TopicPartitionFormat.writes(topicPartition).as[String] -> jsArray
+              }).toMap
+            }
+        )
+      }
+    }
+
+    implicit val ConsumerMessageFormat: Format[ConsumerMessage] = Format(
+      Reads[ConsumerMessage]({
+        case o: JsObject if o ? "KafkaConsumerError" => KafkaConsumerErrorFormat.reads(UnWrapper("KafkaConsumerError", o))
+        case o: JsObject if o ? "CreateConsumer" => CreateConsumerFormat.reads(UnWrapper("CreateConsumer", o))
+        case o: JsObject if o ? "ShutdownConsumer" => JsSuccess(ShutdownConsumer)
+        case o: JsObject if o ? "StartConsumer" => StartConsumerFormat.reads(UnWrapper("StartConsumer", o))
+        case o: JsObject if o ? "MoveOffset" => MoveOffsetFormat.reads(UnWrapper("MoveOffset", o))
+        case o: JsObject if o ? "MessagesPayload" => JsError("Cannot read MessagesPayload type")
+      }),
+
+      Writes[ConsumerMessage] {
+        case m: KafkaConsumerError => Wrapper("KafkaConsumerError", KafkaConsumerErrorFormat.writes(m))
+        case m: CreateConsumer => Wrapper("CreateConsumer", CreateConsumerFormat.writes(m))
+        case ShutdownConsumer => Wrapper("ShutdownConsumer", JsObject.empty)
+        case m: StartConsumer => Wrapper("StartConsumer", StartConsumerFormat.writes(m))
+        case m: MoveOffset => Wrapper("MoveOffset", MoveOffsetFormat.writes(m))
+        case m: MessagesPayload[_, _] => Wrapper("MessagesPayload", MessagesPayloadWrites.writes(m))
+      }
+    )
+
+    private[this] def Wrapper(name: String, message: JsValue) = message.as[JsObject] ++ JsObject(Map("ConsumerMessage" -> JsString(name)))
+    private[this] def UnWrapper(name: String, message: JsObject) = JsObject(message.value.filterKeys(_ != "ConsumerMessage"))
+    private implicit class WrapperImplicit(js: JsObject) {
+      def ?(name: String): Boolean = js.value.get("ConsumerMessage").fold(false)(n => n == JsString(name))
+    }
+
+  }
+
+}
---
+++ test/app/models/ConsumerMessagesProperties.scala
@@ -0,0 +1,88 @@
+package app.models
+
+import actors.consumer.ConsumerActor.StringConsumer
+import actors.consumer.{ AtOffset, FromBeginning, FromEnd }
+import models.ConsumerMessages
+import models.ConsumerMessages._
+import org.apache.kafka.clients.consumer.{ ConsumerRecord, ConsumerRecords }
+import org.apache.kafka.common.TopicPartition
+import org.scalacheck.Prop._
+import org.scalacheck.{ Gen, Properties }
+import play.api.libs.json.{ JsArray, JsObject }
+
+import scala.collection.JavaConverters._
+
+class ConsumerMessagesProperties extends Properties(ConsumerMessages.getClass.getName) {
+
+  import ConsumerMessages.jsonformats._
+
+  def writesAndReadsMessage[T <: ConsumerMessage](m: T, equality: (T, T) => Boolean = (t1: T, t2: T) => t1 == t2) = {
+    equality(ConsumerMessageFormat.reads(ConsumerMessageFormat.writes(m)).get.asInstanceOf[T], m)
+  }
+
+  val KafkaConsumerErrorFormat = forAll { (reason: String) =>
+    writesAndReadsMessage[KafkaConsumerError](KafkaConsumerError(reason))
+    writesAndReadsMessage[KafkaConsumerError](KafkaConsumerError(reason, Some(new RuntimeException("oops"))), (t1, t2) => {
+      t1.reason == t2.reason && t1.error.get.getMessage == t2.error.get.getMessage
+    })
+  }
+
+  val CreateConsumerFormat = forAll { (consumerId: String, properties: Map[String, String]) =>
+    val props = new java.util.Properties()
+    properties.foreach({
+      case (k, v) => props.setProperty(k, v)
+    })
+    assert(JavaPropertiesFormat.reads(JavaPropertiesFormat.writes(props)).get == props)
+    writesAndReadsMessage(CreateConsumer(ConsumerConfig(consumerId, props, StringConsumer)))
+  }
+
+  val ShutdownConsumerFormat = all(writesAndReadsMessage(ShutdownConsumer))
+
+  val StartConsumerFormat = forAll { (topic: String, offset: Long) =>
+    writesAndReadsMessage(StartConsumer(topic, FromBeginning))
+    writesAndReadsMessage(StartConsumer(topic, FromEnd))
+    writesAndReadsMessage(StartConsumer(topic, AtOffset(offset)))
+  }
+
+  val MoveOffsetFormat = forAll { (topic: String, partition: Int, offset: Long) =>
+    writesAndReadsMessage(MoveOffset(topic, partition, FromBeginning))
+    writesAndReadsMessage(MoveOffset(topic, partition, FromEnd))
+    writesAndReadsMessage(MoveOffset(topic, partition, AtOffset(offset)))
+  }
+
+  val MessagesPayloadFormat = forAll { (topics: List[String], partitions: List[Int], keys: List[String]) =>
+    val offsetGen = Gen.chooseNum(1L, 10000L)
+    val valueGen = Gen.alphaStr
+    val keyValues = keys.map(k => k -> valueGen.sample.get).toMap
+    val topicPartitions = topics.flatMap(t => partitions.map(p => new TopicPartition(t, p)))
+    val cr = topicPartitions.map(tp => {
+      tp -> keyValues.map(kv => new ConsumerRecord[String, String](tp.topic(), tp.partition(), offsetGen.sample.get, kv._1, kv._2)).toList.asJava
+    }).toMap
+    val mp = MessagesPayload(new ConsumerRecords[String, String](cr.asJava))
+
+    val json = ConsumerMessageFormat.writes(mp).as[JsObject]
+    val payloadObject = json.value("payload").as[JsObject]
+    val shouldNotBeAbleToRead = all(ConsumerMessageFormat.reads(json).isError) :| "Can not use Reads on MessagePayload"
+    val topicPartitionsAreKeys = all(topicPartitions.forall(tp => payloadObject.value.keySet.contains(TopicPartitionFormat.writes(tp).as[String]))) :| "All TopicPartitions are used as keys"
+
+    val hasAllConsumerRecords = all {
+      payloadObject.value.keySet.forall(topicPartition => {
+        val jsonKeyValues = payloadObject.value(topicPartition).as[JsArray].value.map(_.as[JsObject])
+        jsonKeyValues.forall(obj => {
+          keyValues(obj.value("key").as[String]) == obj.value("value").as[String] && obj.value("offset").as[Long] >= 1L && obj.value("offset").as[Long] <= 10000L
+        })
+      })
+    } :| "All ConsumerRecords mapped to JSON"
+
+    all(shouldNotBeAbleToRead, topicPartitionsAreKeys, hasAllConsumerRecords)
+  }
+
+  property("ConsumerMessageFormat") = all(
+    KafkaConsumerErrorFormat :| "KafKaConsumerError",
+    CreateConsumerFormat :| "CreateConsumer",
+    ShutdownConsumerFormat :| "ShutdownConsumer",
+    StartConsumerFormat :| "StartConsumer",
+    MoveOffsetFormat :| "MoveOffsetFormat",
+    MessagesPayloadFormat :| "MessagesPayloadWrites"
+  )
+}
